613bbd79715032750dd5e3d9e3ecbfa761f344a9
liveskai <54350655+liveskai@users.noreply.github.com>
2023-12-07 01:56:21 +0800

    改变位置

--- 开服/开服.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:loop
-echo (%time%) ttf2 server started.
-echo n| start /wait NorthstarLauncher.exe -dedicated -multiple
-echo (%time%) WARNING: Server closed or crashed, restarting....
-timeout 10
-goto loop
